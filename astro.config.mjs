124faf1bf370c274a797621f3b592b36b44abf1e
WP Infusion <info@wpinfusion.com>
2024-12-28 16:58:11 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import playformInline from "@playform/inline";
+
+import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		tailwind(),
+		alpinejs(),
+		mdx(),
+		(await import("@playform/inline")).default({
+			Critters: true,
+		}),
+	],
+	output: "static",
+	devToolbar: {
+		enabled: false,
+	},
+	experimental: {
+		svg: true,
+	},
+});
